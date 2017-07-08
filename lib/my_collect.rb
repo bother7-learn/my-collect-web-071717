2872f63395cc42120d6dd6a7fc5a7ff9b131276a
Joe Cha <josephmcha@gmail.com>
2017-07-07 20:20:12 -0400

    Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
-
-
+def my_collect(array)
+count = 0
+list = []
+##if array.include?(" ")
+while count < array.length
+  list[count] = yield(array[count])
+count += 1
+end
+##while count < array.length
+##  yield(array[count].split(" ").first)
+##count += 1
+##end
+##else
+##while count < array.length
+##  yield(array[count].uppercase)
+##  count +=1
+##end
+## end
+list
+end
